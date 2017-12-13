aab6911b211e537ab8b2fb3fa189b528ed9f9f3f
Aditya Sanghi <adityasan92@gmail.com>
2017-12-13 05:50:38 -0500

    data analysis

---
+++ data_analysis/rewardParser.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var fs = require('fs');
+var graphLength = 150000;
+
+
+function parseFile(fileName, cb){
+
+    fs.readFile(folderName + fileName, 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        var linesParser = data.split('dtype=float32)]');;
+        //console.log(res);
+        //console.log(linesParser);
+        var x = [];
+        var y = [];
+        console.log(linesParser[0].length);
+        for(var i =0; i<linesParser.length;i++){
+            linesParser[i] = linesParser[i] + 'dtype=float32)]'; 
+            //console.log(typeof(linesParser[i]));
+            //console.log(linesParser[i]);
+            var datapointParser = linesParser[i].split(',[');
+            datapointParser[1] = datapointParser[1].replace("array(", '[')
+            datapointParser[1] = datapointParser[1].split(' dtype=float32), array(').join('')
+            datapointParser[1] = datapointParser[1].split(', dtype=float32)').join('')
+            datapointParser[1] = datapointParser[1].split('0.').join('0.0')
+            console.log(datapointParser[1]);
+            var array = JSON.parse(datapointParser[1]);
+            if(Number(datapointParser[0]) > 150000){
+                break;
+            }
+            if(Number(datapointParser[0]) != 0 && Number(datapointParser[1]) !=0){
+                 x.push(Number(datapointParser[0]));
+                 y.push(array);
+            }
+           
+        }
+        cb(x,y);
+
+    });
+
+}
+
+function checker(files){
+    if(counter > files.length - 1  ){
+        console.log(dataMap); 
+        var json = JSON.stringify(dataMap);
+        fs.writeFile('concreteDropout_rewards.json', json, 'utf8');
+    }
+}
+var folderName = './ConcreteDropout_Reward/rewards/';
+var files = fs.readdirSync(folderName);
+console.log(files);
+var counter = 0; 
+var dataMap = {};
+for(let i=0; i<files.length;i++){
+    dataMap[i] = {};
+    parseFile(files[i], (x, y)=>{
+        dataMap[Number(i)]["x"] =  x;
+        dataMap[Number(i)]["y"] =  y;
+        counter = counter +1
+        checker(files);
+    })
+}
---
+++ data_analysis/vanillaParser.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var fs = require('fs');
+var graphLength = 100000;
+
+
+function parseFile(fileName, cb){
+
+    fs.readFile(folderName + fileName, 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        //var linesParser = data.split('\n');;
+        var linesParser = data.split("\n")
+        //console.log(res);
+        //console.log(linesParser);
+        var x = [];
+        var y = [];
+        var counter = 0;
+        for(var i =0; i<linesParser.length;i++){
+            //console.log(linesParser[i]);
+            if(linesParser[i].search(/ACCURACY:/) >= 0){
+                counter = counter + 500
+                var datapointParser = linesParser[i].split('ACCURACY:');
+                if(counter > 100000){
+                    break;
+                }
+                if(Number(datapointParser[1]) !=0){
+                    x.push(Number(counter));
+                    y.push(Number(datapointParser[1]));
+                }
+            }
+        }
+        cb(x,y);
+
+    });
+
+}
+
+function checker(files){
+    if(counter > files.length - 1  ){
+        console.log(dataMap); 
+        var json = JSON.stringify(dataMap);
+        fs.writeFile('vanilla_acc.json', json, 'utf8');
+    }
+}
+
+var folderName = './vanilla/';
+var files = fs.readdirSync(folderName);
+console.log(files);
+var counter = 0; 
+var dataMap = {};
+for(let i=0; i<files.length;i++){
+    dataMap[i] = {};
+    parseFile(files[i], (x, y)=>{
+        dataMap[Number(i)]["x"] =  x;
+        dataMap[Number(i)]["y"] =  y;
+        counter = counter +1
+        checker(files);
+    })
+}
